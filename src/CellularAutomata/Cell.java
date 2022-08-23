3d281ad67d24fa52ed27af09dfb9c918c282be9e
Rachel MacDougall <macdougallrachel03@gmail.com>
2022-08-22 21:37:30 -0700

    Commit #3

--- src/CellularAutomata/Cell.java
+++ src/CellularAutomata/Cell.java
@@ -1,6 +1,9 @@
 package CellularAutomata;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Cell {
     // Creating instance variables: strength, id, x and y.
@@ -10,6 +13,10 @@ public class Cell {
     private int x;
     private int y;
 
+    // Creating a HashMap instance variable to contain the concentration of various chemicals in a cell
+    private HashMap<String, Integer> chemical_concentration;
+    private Set<String> signal_molecules;
+
     public Cell(int strength, int id, int x, int y) {   // Creating the constructor
         // Calling setter methods
         setStrength(strength);
@@ -17,6 +24,10 @@ public class Cell {
         setX(x);
         setY(y);
 
+        chemical_concentration = new HashMap<>();    // Constructing the HashMap
+        setChemicalConcentration(); // Default constructor for the HashMap
+
+        signal_molecules = new HashSet<>();
     }
     public Cell() {
         this(0,0,0,0);  // Setting the values of the default constructor to 0 for strength, id, x and y
@@ -42,6 +53,13 @@ public class Cell {
         this.y = Math.max(y, 0);
     }
 
+    // Should the following method be static?
+    public void setChemicalConcentration () {   // Sets the list of chemicals all with the default value of 0
+        chemical_concentration.put("Lactate", 0);
+        chemical_concentration.put("ATP", 0);
+        chemical_concentration.put("Glucose", 0);
+    }
+
     public int getStrength() {  // Getter for strength
         return this.strength;
     }
@@ -58,10 +76,59 @@ public class Cell {
         return this.y;
     }
 
+    public int getChemicalConcentration (String chemical) {  // Method which returns the concentration
+                                                             // of an inputted chemical
+        return chemical_concentration.get(chemical);
+    }
+
+    public void replaceChemicalConcentration (String chemical, int concentration) { // Method which replaces a
+                                                                                    // concentration for a different one
+        chemical_concentration.replace(chemical, concentration);
+    }
+
+    public void addSignal (String signal) {     // Method which adds a signal to the HashSet.
+                                                // Declaring the signal molecule is present and therefore ON.
+        signal_molecules.add(signal);
+    }
+
+    public void removeSignal (String signal) {  // Method which deletes a signal from the HashSet.
+                                                // Declaring the signal is OFF.
+        signal_molecules.remove(signal);
+    }
+
+    public void printSignals () {
+        for (String item : signal_molecules) {
+            System.out.println(item);
+        }
+    }
+
     // Creating public method called interactNeighbors
     // Input: ArrayList<Cell> called neighbors
     // Output: Currently nothing
     public void interactNeighbors(ArrayList<Cell> neighbors) {
+        if (TooManyNeighbors(neighbors) == 0) {
+            System.out.println("Die!");
+        }
+        else if (TooManyNeighbors(neighbors) == 1) {
+            System.out.println("Move!");
+        }
+        else {
+            System.out.println("Do nothing.");
+        }
+    }
 
+    private int TooManyNeighbors(ArrayList<Cell> neighbors) {
+        int response = 2;
+        if (neighbors.size() == 8) {    // There are 8 eight neighbors.
+                                        // There is no space for the cell to move, so it should die.
+            response = 0;   // 0 tells interactNeighbors that the cell should die.
+        }
+        else if (neighbors.size() >= 6) {   // There are less than 8 neighbors, but more than 6 neighbors,
+                                            // so the cell should move.
+            response = 1;   // 1 tells interactNeighbors that the cell should move.
+        }
+
+        return response;    // If neither of the previous conditions are met, response remains at 2, which indicates
+                            // to interactNeighbors that nothing should happen.
     }
 }
--- src/CellularAutomata/Main.java
+++ src/CellularAutomata/Main.java
@@ -1,5 +1,7 @@
 package CellularAutomata;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Cell RachelCell = new Cell();
@@ -8,16 +10,27 @@ public class Main {
         System.out.println(RachelCell.getX());
         System.out.println(RachelCell.getY());
 
-        RachelCell.setX(-9);
-        RachelCell.setY(4);
-        System.out.println(RachelCell.getX());
-        System.out.println(RachelCell.getY());
+        System.out.println(RachelCell.getChemicalConcentration("ATP"));
+        RachelCell.replaceChemicalConcentration("ATP", 3);
+        System.out.println(RachelCell.getChemicalConcentration("ATP"));
 
-        Cell OtherCell = new Cell(7, -1, -1, -1);
-        System.out.println(OtherCell.getStrength());
-        System.out.println(OtherCell.getId());
-        System.out.println(OtherCell.getX());
-        System.out.println(OtherCell.getY());
+        RachelCell.addSignal("Estrogen");
+        RachelCell.addSignal("Testosterone");
+        RachelCell.printSignals();
+        RachelCell.removeSignal("Estrogen");
+        RachelCell.printSignals();
+
+        ArrayList<Cell> thing = new ArrayList<Cell>();
+        thing.add(new Cell());
+        thing.add(new Cell(1,1,1,1));
+        thing.add(new Cell(1,1,1,2));
+        thing.add(new Cell(1,1,1,1));
+        thing.add(new Cell(1,1,1,3));
+        thing.add(new Cell(1,1,1,1));
+        thing.add(new Cell(1,1,1,1));
+        thing.add(new Cell(1,1,1,1));
+        RachelCell.interactNeighbors(thing);
+
     }
 }
 
